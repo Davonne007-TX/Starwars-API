56cdc912d8f0d53e8f5b9cb3f3a191495bb86cea
Vonney <davonnejv@gmail.com>
2023-10-24 13:21:59 -0500

    adding routes to about page and planeets page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import About from "./components/About";
+import Planets from "./components/Planets";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/planets" element={<Planets />} />
     </Routes>
   );
 }
