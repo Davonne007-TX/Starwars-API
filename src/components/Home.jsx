77793ad80f093c6a6399f649b742bb621befb353
Vonney <davonnejv@gmail.com>
2023-10-24 13:16:32 -0500

    adding styles to my NavBar and h1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
-import "./css/Home.css";
+import "./css/Home-NavBar.css";
 import NavBar from "./NavBar";
 
 export default function Home() {
   return (
-    <div className="vader">
-      <NavBar />
-    </div>
+    <>
+      <div className="vader">
+        <div className="header-container">
+          <h1>Star Wars Planets</h1>
+          <NavBar />
+        </div>
+      </div>
+    </>
   );
 }
