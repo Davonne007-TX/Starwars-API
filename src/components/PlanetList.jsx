865f3741d9a8a6823cd29523ba55cbc79b65d866
Vonney <davonnejv@gmail.com>
2023-10-26 20:15:43 -0500

    adding back the async function to fetch the api for the planets

--- src/components/PlanetList.jsx
+++ src/components/PlanetList.jsx
@@ -1,9 +1,25 @@
 import NavBar from "./NavBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./css/Planets.css";
 
 export default function PlanetsDetails() {
+  const [planet, setPlanets] = useState([]);
+
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      try {
+        const response = await fetch("http://swapi.dev/api/planets");
+        const starWarsPlanets = await response.json();
+        setPlanets(starWarsPlanets.results);
+        console.log("Star Wars Planets:", starWarsPlanets);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    fetchPlanets();
+  }, []);
+
   return (
     <>
       <div className="header">
